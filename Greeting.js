97380d819c0bca1087c863ea692f695a64c5582c
play.js <git@gituser.com>
2022-06-08 22:30:01 -0700

    some changes

---
+++ Greeting.js
@@ -0,0 +1,18 @@
+import React from 'react';
+//import { useState, useEffect } from 'react';
+
+const Greeting = () => {
+  //const [someThing, setSomeThing] = useState(theThing ? theThing : []);
+  
+//  useEffect(() => {
+//    setSomeThing = ['one', 'two'];
+//  })
+
+    return(
+    <div>
+      <p>what?</p>
+    </div>
+  );
+}
+
+export default Greeting;
